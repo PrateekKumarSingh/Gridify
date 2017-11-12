1d5f69a306b588362b3c8fb4bf63f66a6e280a4d
Prateek Kumar Singh <prateeksingh1590@gmail.com>
2017-11-13 04:37:36 +0530

    init

--- Gridify.psm1
+++ Gridify.psm1
@@ -4,10 +4,5 @@ Get-ChildItem "$PSScriptRoot\Source\" | ForEach-Object {
    . $_.FullName
 }
 
-#$ProcessID = 1..10 | ForEach-Object { (Start-Process cmd -PassThru).ID }
-#Set-ApplicationGrid -ProcessID $ProcessID -Custom "**,*****,***"
-
-# Export only the functions using PowerShell standard verb-noun naming.
-# Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
-# This improves performance of command discovery in PowerShell.
+# Exporting the members and their aliases
 Export-ModuleMember -Function "*-*" -Alias *
