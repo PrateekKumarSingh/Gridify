394541788499c8ae1ad5f7e8a7ecbb047697b873
Prateek Kumar Singh <prateekkumar.singh@markit.com>
2017-11-13 18:41:54 +0530

    included install file for Powershell v4 or earlier

---
+++ Install.ps1
@@ -0,0 +1,71 @@
+<#
+    .SYNOPSIS
+        Download the module files from GitHub.
+
+    .DESCRIPTION
+        Download the module files from GitHub to the local client in the module folder.
+#>
+
+[CmdLetBinding()]
+Param (
+    [ValidateNotNullOrEmpty()]
+    [String]$ModuleName = 'Gridify',
+    [String]$InstallDirectory,
+    [ValidateNotNullOrEmpty()]
+    [String]$GitPath = 'https://github.com/PrateekKumarSingh/Gridify/tree/master/'
+)
+
+Begin {
+    Try {
+        Write-Verbose "$ModuleName module installation started"
+
+        $Files = @(
+            'Gridify.psd1',
+            'Gridify.psm1',
+            'README.md',
+            'Source\MoveApplication.ps1',
+            'Source\Set-GridLayout.ps1'
+        )
+    }
+    Catch {
+        throw "Failed installing the module in the install directory '$InstallDirectory': $_"
+    }
+}
+
+Process {
+    Try {
+        if (-not $InstallDirectory) {
+            Write-Verbose "$ModuleName no installation directory provided"
+
+            $PersonalModules = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+
+            if (($env:PSModulePath -split ';') -notcontains $PersonalModules) {
+                Write-Warning "$ModuleName personal module path '$PersonalModules' not found in '`$env:PSModulePath'"
+            }
+
+            if (-not (Test-Path $PersonalModules)) {
+                Write-Error "$ModuleName path '$PersonalModules' does not exist"
+            }
+
+            $InstallDirectory = Join-Path -Path $PersonalModules -ChildPath $ModuleName
+            Write-Verbose "$ModuleName default installation directory is '$InstallDirectory'"
+        }
+
+        if (-not (Test-Path $InstallDirectory)) {
+            New-Item -Path $InstallDirectory -ItemType Directory -EA Stop | Out-Null
+            Write-Verbose "$ModuleName created module folder '$InstallDirectory'"
+        }
+
+        $WebClient = New-Object System.Net.WebClient
+
+        $Files | ForEach-Object {
+            $WebClient.DownloadFile("$GitPath/$_","$installDirectory\$_")
+            Write-Verbose "$ModuleName installed module file '$_'"
+        }
+
+        Write-Verbose "$ModuleName module installation successful"
+    }
+    Catch {
+        throw "Failed installing the module in the install directory '$InstallDirectory': $_"
+    }
+}
